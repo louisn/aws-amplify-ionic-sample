98b26c42773c12f29946c269e99d3ed7c7b79eb9
Louis Naber <louis.naber@gmail.com>
2019-05-30 11:17:15 -0500

    Using one identity configuration and logging in to APV.

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,34 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import Amplify, { Analytics } from 'aws-amplify';
-import aws_exports from './aws-exports';
+import Amplify from 'aws-amplify';
+// import aws_exports from './aws-exports';
 
-Amplify.configure(aws_exports);
+// Amplify.configure(aws_exports);
+Amplify.configure({
 
+  Auth: {
+    mandatorySignIn: true,
+        region: 'us-east-1',
+        userPoolId: 'us-east-1_QiLjC77MM',
+        identityPoolId: 'us-east-1:6c65e17a-6c86-445e-9958-6a8b156a6d65',
+        userPoolWebClientId: '4onrn29c8jts4o7ros3ssvh6vq'
+  },
+  Storage: {
+    region: '',
+        bucket: '',
+        identityPoolId: ''
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'apv',
+        endpoint: 'https://817klbu9si.execute-api.us-east-1.amazonaws.com/DEV',
+        region: 'us-east-1'
+      },
+    ]
+  }
+});
 if (environment.production) {
   enableProdMode();
 }
